a0180125425d9e22c2abae4c8619c767c7fa070f
Ganesh Ralla <ganeshrtist@gmail.com>
2026-05-17 08:36:58 +0530

    got the products

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,7 @@ from . import  views
 
 
 urlpatterns = [
-    path('get-products/',views.get_products),
+    # path('get-products/',views.get_products),
     path('products',views.products_view,name='product'),
     path('products/<int:pk>',views.view_single_product,name='single_product')
 ]
