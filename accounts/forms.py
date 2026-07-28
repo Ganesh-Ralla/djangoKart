18af47406d99f099c69c5bd5a4f139b750aaa927
Ganesh Ralla <ganeshrtist@gmail.com>
2026-07-28 21:31:54 +0530

    forms style updated

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,29 +1,41 @@
 from django import forms
 from django.contrib.auth.models import User
 
+style_class="p-2 rounded-xl border border-gray-200 w-full outline-0 mb-3 bg-white"
 class UserForm(forms.ModelForm):
+    
+    
     class Meta:
         model = User
         fields = ['first_name','last_name','email','username','password']
 
+        
+
         widgets = {
             'first_name':forms.TextInput(attrs={
                 'placeholder':'first name',
+                'class':style_class,
             }),
             'last_name':forms.TextInput(attrs={
-                'placeholder':'last name'
+                'placeholder':'last name',
+                'class':style_class,
             }),
             'email':forms.EmailInput(attrs={
-                'placeholder':'email'
+                'placeholder':'email',
+                'class':style_class,
             }),
             'username':forms.TextInput(attrs={
-                'placeholder':'username'
+                'placeholder':'username',
+                'class':style_class,
             }),
             'password':forms.PasswordInput(attrs={
-                'placeholder':'password'
+                'placeholder':'password',
+                'class':style_class,
             })
 
         }
+
+        
     
     def save(self,commit=True):
         user = User.objects.create_user(
